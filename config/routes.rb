e050efbdf9bbe64d30daad0d0253e288b069b5d7
Kyle Anderson <kra.aue@gmail.com>
2019-08-25 08:56:46 -0400

    Add participant
    The participant class has been added. No routes or anything yet, but
    once the roster class is in it will receive routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+# frozen_string_literal: true
 
+Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   get 'help', to: 'static_pages#help'
